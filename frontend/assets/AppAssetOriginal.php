2f360c72c17a8c319db44bbcf5a50418f0dc5fa4
roberson <robersom@gmail.com>
2026-03-05 16:15:58 -0300

    to revert to using Bootstrap/Tailwind

---
+++ frontend/assets/AppAssetOriginal.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace frontend\assets;
+
+use yii\web\AssetBundle;
+
+//=================================================================================================================================
+// To revert to using Bootstrap, delete the AppAsset.php file, copy the AppAssetOriginal.php file, and rename it to AppAsset.php.
+// To revert to using Tailwind, delete the AppAsset.php file, copy the AppAssetTailwind.php file, and rename it to AppAsset.php.
+//=================================================================================================================================
+// Para voltar a usar o bootstrap, apague o arquivo AppAsset.php, copie o AppAssetOriginal.php e o renomeie para AppAsset.php
+// Para voltar a usar o Tailwind, apague o arquivo AppAsset.php, copie o AppAssetTailwind.php e o renomeie para AppAsset.php
+//=================================================================================================================================
+
+/**
+ * Main frontend application asset bundle.
+ */
+class AppAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        'css/site.css',
+    ];
+    public $js = [];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap5\BootstrapAsset',
+    ];
+}
